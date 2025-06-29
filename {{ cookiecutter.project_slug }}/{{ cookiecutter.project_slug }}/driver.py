918f3f870ded9e5f9b221ce26de7432d677bd75a
Algopaul <72927083+Algopaul@users.noreply.github.com>
2025-06-29 15:02:43 -0400

    add driver duration

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/driver.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/driver.py
@@ -1,5 +1,7 @@
 import logging
+import time
 
+import humanize
 import hydra
 from omegaconf import OmegaConf
 from {{cookiecutter.project_slug}}.config import Config
@@ -7,7 +9,12 @@ from {{cookiecutter.project_slug}}.config import Config
 
 @hydra.main(version_base=None, config_name='config', config_path='../conf')
 def main(cfg: Config) -> None:
+  t0 = time.time()
   logging.info("\n%s", OmegaConf.to_yaml(cfg))
+  logging.info(
+      'Driver complete. Took %s',
+      humanize.naturaldelta(time.time() - t0, minimum_unit='microseconds'),
+  )
 
 
 if __name__ == "__main__":
